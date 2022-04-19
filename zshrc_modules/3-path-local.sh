ec12553c824f6ed092a17f331488e3999dc712c5
Watanabe Takafumi <dieu.detruit@gmail.com>
2022-04-19 10:48:21 +0900

    ignore local settings

--- zshrc_modules/3-path-local.sh
+++
@@ -1,9 +0,0 @@
-export PATH="$HOME/tools/golang/go1.17.1/bin:$PATH"
-export PATH="/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin:$PATH"
-export PATH="$HOME/tools/android-studio/bin:$PATH"
-export PATH="$HOME/tools/flutter/bin:$PATH"
-export PATH="$HOME/tools/golang/go1.17.1/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/tools/sidequest/SideQuest-0.10.24:$PATH"
-
-export GOPATH="$HOME/tools/golang/go1.17.1/"
