6b12b31275a0030e0b850bd1f9bb1a3b5ae46c5f
Watanabe Takafumi <dieu.detruit@gmail.com>
2025-03-26 21:59:49 +0900

    added pnpm to path

--- zshrc_modules/2-path.sh
+++ zshrc_modules/2-path.sh
@@ -2,8 +2,13 @@ export PATH="/usr/local/opt/llvm/bin:$PATH"
 export PATH="$HOME/.local/bin/:$PATH"
 export PATH="/usr/local/cuda/bin:$PATH"
 export PATH="$HOME/tools/bin:$PATH"
+export PATH="$HOME/.local/share/pnpm:$PATH"
 
 export DYLD_LIBRARY_PATH="/usr/local/cuda/lib64:$DYLD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 
 export PATH="$HOME/.poetry/bin:$PATH"
+
+export EDITOR="nvim"
+
+export PNPM_HOME="$HOME/.local/share/pnpm"
